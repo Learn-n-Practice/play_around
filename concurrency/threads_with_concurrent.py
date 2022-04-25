0e298313dc0e517ecc8d8e70c6f77463506da4c3
Godfred Doe <godfredoe9@gmail.com>
2022-04-25 20:21:12 +0000

    mapping the threads

--- concurrency/threads_with_concurrent.py
+++ concurrency/threads_with_concurrent.py
@@ -4,6 +4,7 @@ from time import sleep, perf_counter
 
 secs = input("Seconds to sleep function separated by ';' : ")
 secs = secs.split(";")
+secs = map(lambda x: int(x), secs)
 
 start = perf_counter()
 
@@ -16,10 +17,19 @@ def sleep_around(sec):
     return f"Done sleeping...{sec}"
 
 # Create and execute thread with ThreadPoolExecutor 
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    results = [executor.submit(sleep_around, int(sec)) for sec in secs]
-    for f in concurrent.futures.as_completed(results):
-        print(f.result())
+# with concurrent.futures.ThreadPoolExecutor() as executor:
+#     results = [executor.submit(sleep_around, sec) for sec in secs]
+#     for f in concurrent.futures.as_completed(results):
+#         print(f.result())
+
+
+# Using the map method
+with concurrent.futures.ThreadPoolExecutor() as mapper:
+    print("\n\nMapping threads")
+    results = mapper.map(sleep_around, secs)
+
+    for result in results:
+        print(result)
 
 finish = perf_counter()
 print(f"Done execunting in {round(finish-start, 2)} seconds")
