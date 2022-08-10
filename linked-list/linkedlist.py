4e4336792311b8b4b717cf0da387d010650f139c
Godfred Doe <godfredoe9@gmail.com>
2022-08-10 00:18:25 +0000

    changed node value name from item to data

--- linked-list/linkedlist.py
+++ linked-list/linkedlist.py
@@ -1,15 +1,15 @@
 class Node:
-    def __init__(self, item=None, next=None):
-        self.item = item
+    def __init__(self, data=None, next=None):
+        self.data = data
         self.next = next
 
-    def get_item(self):
-        return self.item
+    def get_data(self):
+        return self.data
 
     def get_next(self):
         return self.next
 
-    def set_item(self, item):
+    def set_data(self, item):
         self.item = item
 
     def set_next(self, next):
